6c19949d549e5d106f695bede1e9d27e34cb97be
Nate Kohari <nkohari@gmail.com>
2017-07-30 19:31:43 -0400

    Tweaks

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,12 @@ class App extends React.Component {
       <div className='app'>
         <div className='splash'>
           <div className='splash-animation'>
+            <Svg src={require('assets/reticle.svg')} className='reticle' />
             <Svg src={require('assets/ring0.svg')} className='ring0' />
             <Svg src={require('assets/circuit0.svg')} className='circuit0' />
             <Svg src={require('assets/circuit1.svg')} className='circuit1' />
             <Svg src={require('assets/ring1.svg')} className='ring1' />
             <Svg src={require('assets/ring2.svg')} className='ring2' />
-            <Svg src={require('assets/ring3.svg')} className='ring3' />
             <Svg src={require('assets/wave0.svg')} className='wave0' />
             <Svg src={require('assets/boxes.svg')} className='boxes' />
           </div>
